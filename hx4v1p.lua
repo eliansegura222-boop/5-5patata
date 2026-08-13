9824e22561f2951bc33e9ec8d93cd9e7e7778a22
Hexa <eliansegura222@gmail.com>
2026-08-12 21:25:15 -0400

    Update hx4v1p.lua

--- hx4v1p.lua
+++ hx4v1p.lua
@@ -1,15 +1,15 @@
 return {
-    ["3108101113"] = {
+    ["3108101113"] = { --zen
         vip = true,
         expires = "permanent"
     },
 
-    ["11486227663"] = {
+    ["11486227663"] = { --bober2
         vip = true,
         expires = "permanent"
     },
 
-    ["3693598316"] = {
+    ["3693598316"] = { --fenix
         vip = true,
         expires = "permanent"
     }
